9ab0976333982fae46b4c5440e410578756e51b9
juan pablo <darkcode@DESKTOP-H52600F>
2023-08-09 00:46:28 -0500

    Clases objetos y atributos

---
+++ Clases-y-objetos-1.py
@@ -0,0 +1,15 @@
+#la clase es la receta que debemos seguir para construir un objeto
+#un objeto es el producto de la clase
+ 
+#sintaxis pascalcase
+class Celular():
+    #atributos estaticos que no cambian
+    marca = "Samsung"
+    modelo = "s23"
+    camara = "48MP"
+
+#instancia de la clase
+celular1 = Celular()
+print(celular1.marca)
+print(celular1.modelo)
+print(celular1.camara)
---
+++ Clases-y-objetos-Atributos.py
@@ -0,0 +1,19 @@
+#atributos
+
+class Celular():
+
+    #funcion iniciador
+    def __init__(self,marca,modelo,camara) -> None:
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+    #funcion str retorna un print
+    def __str__(self) -> str:
+        return f"La marca es: {self.marca}, el modelo es: {self.modelo} y la camara es de: {self.camara} "
+
+celular1 = Celular("Samsung","J2 Prime", "28MP")
+print(celular1)
+celular2 = Celular("phone","12 pro", "32MP")
+print(celular2)
+celular3 = Celular("Xiaomi","Poco x3 pro", "48MP")
+print(celular3)
